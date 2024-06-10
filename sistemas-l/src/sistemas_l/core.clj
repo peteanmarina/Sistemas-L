418c75b1315f016aee2a63d9df03a64234e3bab9
nicorf12 <nromano@fi.uba.ar>
2024-06-10 16:04:43 -0300

    errores de pila

--- sistemas-l/src/sistemas_l/core.clj
+++ sistemas-l/src/sistemas_l/core.clj
@@ -27,7 +27,13 @@
 (defn pen-down [turtle]
   (assoc turtle :pen-down? true))
 
-;Falta pila
+(defn push-state [turtle]
+  (update turtle :stack conj (select-keys turtle [:x :y :angle :pen-down?])))
+
+(defn pop-state [turtle]
+  (let [last-state (peek (:stack turtle))
+        new-stack (pop (:stack turtle))]
+    (merge turtle last-state {:stack new-stack})))
 
 (defn parse-l-system-file [filename]
   (let [lines (str/split-lines (slurp filename))
@@ -60,7 +66,8 @@
            \+ [:right angle]
            \- [:left angle]
            \| [:invert]
-           ;Falta pila
+           \[ [:push]
+           \] [:pop]
            nil))
        l-system))
 
@@ -72,7 +79,8 @@
       :right (turn-right turtle arg)
       :left (turn-left turtle arg)
       :invert (invert-direction turtle)
-      ;Falta pila
+      :push (push-state turtle)
+      :pop (pop-state turtle)
       turtle)))
 
 (defn execute-commands [turtle commands]
@@ -80,15 +88,23 @@
             (conj acc (execute-command (last acc) cmd)))
           [turtle] commands))
 
+(defn calculate-bounds [turtle-positions]
+  (let [xs (map :x turtle-positions)
+        ys (map :y turtle-positions)
+        min-x (apply min xs)
+        max-x (apply max xs)
+        min-y (apply min ys)
+        max-y (apply max ys)]
+    {:min-x min-x :max-x max-x :min-y min-y :max-y max-y}))
+
 (defn to-svg [turtle-positions]
-  (let [lines (for [[t1 t2] (partition 2 1 turtle-positions)
+  (let [{:keys [min-x max-x min-y max-y]} (calculate-bounds turtle-positions)
+        width (- max-x min-x)
+        height (- max-y min-y)
+        lines (for [[t1 t2] (partition 2 1 turtle-positions)
                     :when (:pen-down? t1)]
-                (str "<line x1=\"" (:x t1) "\" y1=\"" (:y t1)
-                     "\" x2=\"" (:x t2) "\" y2=\"" (:y t2)
-                     "\" stroke-width=\"0.2\" stroke=\"black\"/>"))]
-    (str "<svg viewBox=\"-50 -150 300 200\" xmlns=\"http://www.w3.org/2000/svg\">"
-         (str/join "" lines)
-         "</svg>")))
+                (str "<line x1=\"" (:x t1) "\" y1=\"" (:y t1) "\" x2=\"" (:x t2) "\" y2=\"" (:y t2) "\" stroke-width=\"0.1\" stroke=\"black\"/>"))]
+    (str "<svg viewBox=\"" min-x " " min-y " " width " " height "\" xmlns=\"http://www.w3.org/2000/svg\">" (str/join "" lines) "</svg>")))
 
 (defn generate-fractal-svg [filename iterations output-filename]
   (let [{:keys [angle axiom rules]} (parse-l-system-file filename)
